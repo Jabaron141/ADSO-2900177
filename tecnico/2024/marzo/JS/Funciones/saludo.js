4c95afb6067e3a18c1ce27fb23a19e20c584dbef
Jabaron141 <161946031+Jabaron141@users.noreply.github.com>
2024-04-01 16:15:49 -0500

    Add files via upload

---
+++ tecnico/2024/marzo/JS/Funciones/saludo.js
@@ -0,0 +1,15 @@
+/**
+ * Función saludo - Hola mundo
+ * Autor: Jorge Baron Corredor
+ * Fecha: Lunes 01 de abril de 2024
+ */
+
+function saludo(psaludo){
+    let saludar = psaludo;
+    return saludar;
+}
+
+const saludoExp = function(psaludo){
+    let saludar = psaludo;
+    return saludar;
+}

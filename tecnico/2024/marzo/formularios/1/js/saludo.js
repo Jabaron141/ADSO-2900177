589c936911f9268f7f9dff910413160acab9c315
SENA <SENA@NEIDFPSITFSD764.FormacionSena.RED>
2024-06-26 17:08:09 -0500

    SENA

---
+++ tecnico/2024/marzo/formularios/1/js/saludo.js
@@ -0,0 +1,7 @@
+function saludar (){
+    let saludo = document.getElementById('txtSaludo').value;
+    document.getElementById('saludo').innerHTML = `<strong>${saludo}</strong>`;
+    return false;
+}
+
+  
---
+++ tecnico/2024/marzo/formularios/2/js/operaciones.js
@@ -0,0 +1,20 @@
+function operaciones (){
+    let num1 = parseInt(document.getElementById('txtNumeroUno').value);
+    let num2 = parseInt(document.getElementById('txtNumeroDos').value);
+    let sumar= num1+num2;
+    let restar= num1-num2;  
+    let multiplicar= num1*num2;
+    let dividir= num1/num2;
+    sumar= num1+num2;
+    restar= num1-num2;  
+    multiplicar= num1*num2;
+    dividir= num1/num2;
+    let rsuma = `Suma: ${sumar} <br>`;
+    let rresta = `Resta: ${restar} <br>`;
+    let rmultiplicar = `Multiplicación: ${multiplicar} <br>`;
+    let rdividir = `División: ${dividir} <br>`
+    document.getElementById('operaciones').innerHTML = rsuma + rresta + rmultiplicar + rdividir;
+    return false;
+}
+
+  
